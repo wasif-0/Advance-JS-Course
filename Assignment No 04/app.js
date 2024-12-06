989f35e6411a5ad4b9a885d96a58132ca70d19bb
DesignDeveloper <111518026+wasif-0@users.noreply.github.com>
2024-12-07 02:17:28 +0500

    Add files via upload

---
+++ Assignment No 04/app.js
@@ -0,0 +1,34 @@
+const arrays = [
+    42,
+    true,
+    "Hello, World!",
+    { name: "John", age: 25, isStudent: false },
+    false,
+    3.14,
+    "JavaScript is fun!",
+  ];
+  
+
+
+//   let stringArr = arrays.map(e => String(e));
+//   console.log(stringArr);
+  
+// let numberArr = arrays.filter(e => typeof e === 'number');
+// console.log(numberArr);
+
+// arrays.forEach(e => console.log(typeof e));
+
+// let sum = arrays.reduce((acc, e) => (typeof e === 'number' ? acc + e : acc), 0);
+// console.log(sum);
+
+// let firstBoolean = arrays.find(e => typeof e === 'boolean');
+// console.log(firstBoolean);
+
+// let objectIndex = arrays.findIndex(e => typeof e === 'object');
+// console.log(objectIndex);
+
+// let hasNumbers = arrays.some(e => typeof e === 'number');
+// console.log(hasNumbers);
+
+// let allStrings = arrays.every(e => typeof e === 'string');
+// console.log(allStrings);
